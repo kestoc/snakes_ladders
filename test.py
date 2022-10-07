bbc4b546daad760d384cd0e4eb42e33ec3a468ab
kestoc <kevin512ocampo@gmail.com>
2022-10-07 12:28:14 -0500

    Plantilla para realizar las pruebas al codigo

--- test.py
+++ test.py
@@ -3,17 +3,19 @@ import snakes_ladders
 import unittest
 
 """
-Autores: Kevin Ocampo - Robin Quintero 
+    Autores: Kevin Ocampo 
 
-Fecha: 9/28/2022
+    Fecha: 10/7/2022
 
-Version: 1.0
+    Descripción: Este archivo contiene las pruebas realizadas al juego 'Serpientes y Escaleras' con Unittest.
 
-Descripción: Archivo de pruebas sobre la tarea 2, con Unittest. Las pruebas a realizar están determinadas 
-             por el Excel adjunto en donde se aplica la técnica de clases de equivalencia.
-
-             Los casos no validos dan Fail al ejecutarlos ya que en las precondiciones se realizan las verificaciones
-             expresadas en el enunciado del ejercicio, por lo cual al no poder ejecutar la prueba por no cumplir con lo que es valido,
-             se da por entendido entonces que la prueba fue exitosa ya que debia fallar. (Lo anterior bajo el supuesto de que no tengo
-             acceso al codigo, entonces no se puede modificar para poner los assert)
+    Version: 1.0
 """
+
+class test1(unittest.TestCase):
+    def setUp(self):
+        print("PreCondiciones")
+    def test(self):
+        print("Ejecucion prueba")
+    def tearDown(self):
+        print("PostCondiciones")
